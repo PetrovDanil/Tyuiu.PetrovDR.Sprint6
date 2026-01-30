ca9d7c5ae3f8b8cde5e6b1d1c9cc6afb8421921f
tokyopium ㅤ <olilhjgjg2@gmail.com>
2026-01-31 03:04:12 +0500

    task4

---
+++ Tyuiu.PetrovDR.Sprint6.Task4.V4.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.PetrovDR.Sprint6.Task4.V4.Lib
+{
+    public class DataService : ISprint6Task4V4
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = (stopValue - startValue) + 1;
+            int c = 0;
+            double[] result = new double[len];
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                result[c] = Math.Round(((2 * (double)i + 6) / (Math.Cos(i) + i)) - 3, 2);
+                if (Math.Cos(i) + i == 0) { result[c] = 0; }
+                c++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint6.Task4.V4/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.PetrovDR.Sprint6.Task4.V4
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
