de6983006a9a3dd31ed1162c00fd614a4e71c499
tokyopium ㅤ <olilhjgjg2@gmail.com>
2026-01-31 03:15:09 +0500

    task5

---
+++ Tyuiu.PetrovDR.Sprint6.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PetrovDR.Sprint6.Task5.V3.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint6.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = $@"C:\DataSprint6\InPutDataFileTask5V3.txt";
+            double[] res = ds.LoadFromDataFile(path);
+            double[] wait = new double[] { 2.5, -2.6, 5.7, -6.8, 9.9 };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint6.Task5.V3/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.PetrovDR.Sprint6.Task5.V3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
